83662212bf2ca11ba002854b3a7d22503538e938
max grigoriev <aix60689100@gmail.com>
2023-02-01 19:45:36 +0200

    done func

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 balance = 5000
-
+log = []
 
 def one_use():
     user_answer = input('Продолжить работу?\ntype exit/press enter\n_')
@@ -8,8 +8,16 @@ def one_use():
 
     return True
 
-log = []
-def set_log():
+def set_log(сhoice):
+    if choice == '1':
+        log.append("Choose currency")
+    elif choice == '2':
+        log.append("Show balance")
+    elif choice == '3':
+        log.append("Put money")
+    elif choice == '4':
+        log.append("Get money")
+
 
 while one_use():
     print('Действия пользователя')
