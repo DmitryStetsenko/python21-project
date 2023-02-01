f54be8214d01e79d12a0b36fdd23d44469dbbd2b
Vladislav Motienko <motienkovlad@gmail.com>
2023-02-01 19:22:39 +0200

    integrated func to main loop

--- main.py
+++ main.py
@@ -27,6 +27,8 @@ def choose_currency():
             print("your balance = ", balance, "usd")
 
 
-
 while one_use():
     print('Действия пользователя')
+    choose_action = input("1 - Choose currency")
+    if choose_currency == "1":
+        choose_currency()
