caafa87735e0f2ad2d08719345e3cc7f112bc28d
Vladislav Motienko <motienkovlad@gmail.com>
2023-02-01 21:34:25 +0200

    fixed function

--- main.py
+++ main.py
@@ -19,20 +19,19 @@ def show_balance():
 
 
 def choose_currency():
+    global current_currency
     global balance
     choose = input("Enter currency: (1 - usd / 2- uah)")
     if choose == "1":
-        if current_currency == "usd":
-            print("your balance = ", balance, "usd")
-        elif current_currency == "uah":
-            balance = balance / 36.93
-            print("your balance = ", balance, "usd")
-    if choose == "2":
         if current_currency == "uah":
-            print("your balance = ", balance, "uah")
-        elif current_currency == "usd":
+            balance = balance / 36.93
+        current_currency = "usd"
+        show_balance()
+    if choose == "2":
+        if current_currency == "usd":
             balance = balance * 36.93
-            print("your balance = ", balance, "uah")
+        current_currency = "uah"
+        show_balance()
 
 
 
