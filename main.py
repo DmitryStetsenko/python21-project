71c51682ec2615d30d53aec0e3c7e8e7e31c6db9
Vladislav Motienko <motienkovlad@gmail.com>
2023-02-01 19:16:00 +0200

    add choose_currency function

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 balance = 5000
+current_currency = "uah"
 
 
 def one_use():
@@ -7,6 +8,24 @@ def one_use():
         return False
 
     return True
+
+
+def choose_currency():
+    global balance
+    choose = input("Enter currency: (1 - usd / 2- uah)")
+    if choose == "1":
+        if current_currency == "usd":
+            print("your balance = ", balance, "usd")
+        elif current_currency == "uah":
+            balance = balance / 36.93
+            print("your balance = ", balance, "uah")
+    if choose == "2":
+        if current_currency == "uah":
+            print("your balance = ", balance, "uah")
+        elif current_currency == "usd":
+            balance = balance * 36.93
+            print("your balance = ", balance, "usd")
+
 
 
 while one_use():
