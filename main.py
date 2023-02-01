2f7eacd18de0b673775cdb4ae60afc5dd5cbcaff
Dmitry Stetsenko <ebusines2010@gmail.com>
2023-02-01 18:51:30 +0200

    create balance, main loop

---
+++ main.py
@@ -0,0 +1,13 @@
+balance = 5000
+
+
+def one_use():
+    user_answer = input('Продолжить работу?\ntype exit/press enter\n_')
+    if user_answer == 'exit':
+        return False
+
+    return True
+
+
+while one_use():
+    print('Действия пользователя')
