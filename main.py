e73f999d5026721541a53a6bfde882eec31107e5
Andrey Khmurchyk <3013415454@gmail.com>
2023-02-01 20:47:03 +0300

    use func

--- main.py
+++ main.py
@@ -18,20 +18,21 @@ def user_unset():
 
 
 
-#
-#
-# def one_use():
-#     user_answer = input('Продолжить работу?\ntype exit/press enter\n_')
-#     if user_answer == 'exit':
-#         return False
-#
-#     return True
-#
-#
-# while one_use():
-#     print('Действия пользователя')
-#
 
+
+def one_use():
+    user_answer = input('Продолжить работу?\ntype exit/press enter\n_')
+    if user_answer == 'exit':
+        return False
+
+    return True
+
+
+while one_use():
+    print('Действия пользователя')
+
+
+user_unset()
 
 
 
