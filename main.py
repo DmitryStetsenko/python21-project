2c3ada74286a498ab12312a187ddcce7677f5345
Andrey Khmurchyk <3013415454@gmail.com>
2023-02-01 20:43:52 +0300

    def user_unseh

--- main.py
+++ main.py
@@ -1,23 +1,37 @@
+
 balance = 5000
 
 
-def one_use():
-    user_answer = input('Продолжить работу?\ntype exit/press enter\n_')
-    if user_answer == 'exit':
-        return False
-
-    return True
-
-
-while one_use():
-    print('Действия пользователя')
-
-
 def user_unset():
-    try:
-        user_unset_sum = input("Введите cумму: ")
-    except ValueError:
-        print("введено не число, оставляем 0")
-        if user_unset_sum <= 0:
-            print("у вас нет столько денег")
+    global balance
+    user_unset_sum = int(input("Введите cумму: "))
+
+    if user_unset_sum <= 0:
+        print("неверная сумма")
+    elif user_unset_sum > balance:
+        print("сумма привышает баланс")
+    else:
+        rest = balance - user_unset_sum
+        balance = rest
+
+        print('деньги успешно сняты \n' + str(balance))
+
+
+
+#
+#
+# def one_use():
+#     user_answer = input('Продолжить работу?\ntype exit/press enter\n_')
+#     if user_answer == 'exit':
+#         return False
+#
+#     return True
+#
+#
+# while one_use():
+#     print('Действия пользователя')
+#
+
+
+
 
